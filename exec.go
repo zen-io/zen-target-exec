587034700f2c7d3afae85258d8be8d33406301f2
Tiago Posse <tiago.posse@hi.health>
2023-06-13 12:57:45 +0200

    moving to OSS github

--- exec.go
+++ exec.go
@@ -5,7 +5,8 @@ import (
 	"os/exec"
 	"strings"
 
-	ahoy_targets "gitlab.com/hidothealth/platform/ahoy/src/target"
+	environs "github.com/zen-io/zen-core/environments"
+	zen_targets "github.com/zen-io/zen-core/target"
 )
 
 type ExecScript struct {
@@ -16,27 +17,36 @@ type ExecScript struct {
 }
 
 type ExecConfig struct {
-	Outs                      []string              `mapstructure:"outs"`
-	BuildCommand              []string              `mapstructure:"build"`
-	Tools                     map[string]string     `mapstructure:"tools"`
-	ScriptCommands            map[string]ExecScript `mapstructure:"scripts"`
-	ExternalPath              *string               `mapstructure:"external_path"`
-	ahoy_targets.BuildFields  `mapstructure:",squash"`
-	ahoy_targets.DeployFields `mapstructure:",squash"`
+	BuildCommand   []string                         `mapstructure:"build"`
+	Srcs           []string                         `mapstructure:"srcs" desc:"Sources for the build"`
+	MappedSrcs     map[string][]string              `mapstructure:"mapped_srcs" desc:"a map of organised srcs"`
+	Outs           []string                         `mapstructure:"outs" desc:"Outs for the build"`
+	ScriptCommands map[string]ExecScript            `mapstructure:"scripts"`
+	ExternalPath   *string                          `mapstructure:"external_path"`
+	Name           string                           `mapstructure:"name" desc:"Name for the target"`
+	Description    string                           `mapstructure:"desc" desc:"Target description"`
+	Labels         []string                         `mapstructure:"labels" desc:"Labels to apply to the targets"` //
+	Deps           []string                         `mapstructure:"deps" desc:"Build dependencies"`
+	PassEnv        []string                         `mapstructure:"pass_env" desc:"List of environment variable names that will be passed from the OS environment, they are part of the target hash"`
+	SecretEnv      []string                         `mapstructure:"secret_env" desc:"List of environment variable names that will be passed from the OS environment, they are not used to calculate the target hash"`
+	Env            map[string]string                `mapstructure:"env" desc:"Key-Value map of static environment variables to be used"`
+	Tools          map[string]string                `mapstructure:"tools" desc:"Key-Value map of tools to include when executing this target. Values can be references"`
+	Visibility     []string                         `mapstructure:"visibility" desc:"List of visibility for this target"`
+	Environments   map[string]*environs.Environment `mapstructure:"environments" desc:"Deployment Environments"`
 }
 
-func (ec ExecConfig) GetTargets(tcc *ahoy_targets.TargetConfigContext) ([]*ahoy_targets.Target, error) {
-	opts := []ahoy_targets.TargetOption{
-		ahoy_targets.WithSrcs(map[string][]string{"_srcs": ec.Srcs}),
-		ahoy_targets.WithOuts(ec.Outs),
-		ahoy_targets.WithVisibility(ec.Visibility),
-		ahoy_targets.WithEnvVars(ec.Env),
-		ahoy_targets.WithTools(ec.Tools),
-		ahoy_targets.WithPassEnv(ec.PassEnv),
-		ahoy_targets.WithEnvironments(ec.Environments),
-		ahoy_targets.WithTargetScript("build", &ahoy_targets.TargetScript{
+func (ec ExecConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_targets.Target, error) {
+	opts := []zen_targets.TargetOption{
+		zen_targets.WithSrcs(map[string][]string{"_srcs": ec.Srcs}),
+		zen_targets.WithOuts(ec.Outs),
+		zen_targets.WithVisibility(ec.Visibility),
+		zen_targets.WithEnvVars(ec.Env),
+		zen_targets.WithTools(ec.Tools),
+		zen_targets.WithPassEnv(ec.PassEnv),
+		zen_targets.WithEnvironments(ec.Environments),
+		zen_targets.WithTargetScript("build", &zen_targets.TargetScript{
 			Deps: ec.Deps,
-			Run: func(target *ahoy_targets.Target, runCtx *ahoy_targets.RuntimeContext) error {
+			Run: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
 				cmds := []string{}
 				for _, cmd := range ec.BuildCommand {
 					interpolatedCmd, err := target.Interpolate(cmd)
@@ -56,7 +66,7 @@ func (ec ExecConfig) GetTargets(tcc *ahoy_targets.TargetConfigContext) ([]*ahoy_
 		if err != nil {
 			return nil, fmt.Errorf("interpolating external path (%s): %w", *ec.ExternalPath, err)
 		}
-		opts = append(opts, ahoy_targets.WithExternalPath(interpolatedPath))
+		opts = append(opts, zen_targets.WithExternalPath(interpolatedPath))
 	}
 
 	for script, execCmd := range ec.ScriptCommands {
@@ -64,7 +74,7 @@ func (ec ExecConfig) GetTargets(tcc *ahoy_targets.TargetConfigContext) ([]*ahoy_
 			return nil, fmt.Errorf("no commands provided for %s", script)
 		}
 
-		ts := &ahoy_targets.TargetScript{
+		ts := &zen_targets.TargetScript{
 			Deps: execCmd.Deps,
 			Run:  getCmd(execCmd.Command),
 		}
@@ -76,19 +86,19 @@ func (ec ExecConfig) GetTargets(tcc *ahoy_targets.TargetConfigContext) ([]*ahoy_
 			ts.Pre = getCmd(execCmd.Post)
 		}
 
-		opts = append(opts, ahoy_targets.WithTargetScript(script, ts))
+		opts = append(opts, zen_targets.WithTargetScript(script, ts))
 	}
 
-	return []*ahoy_targets.Target{
-		ahoy_targets.NewTarget(
+	return []*zen_targets.Target{
+		zen_targets.NewTarget(
 			ec.Name,
 			opts...,
 		),
 	}, nil
 }
 
-func getCmd(scriptCmds []string) func(target *ahoy_targets.Target, runCtx *ahoy_targets.RuntimeContext) error {
-	return func(target *ahoy_targets.Target, runCtx *ahoy_targets.RuntimeContext) error {
+func getCmd(scriptCmds []string) func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
+	return func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
 		target.SetStatus("Executing %s", target.Qn())
 		args := []string{
 			"-c",
