84c02fd1a75665e53c2efe7e0de8b3902422eeab
Tiago Posse <tiago.posse@hi.health>
2023-08-20 19:09:50 +0200

    use targetbuilder

--- exec.go
+++ exec.go
@@ -1,62 +1,56 @@
 package exec
 
 import (
+	b64 "encoding/base64"
 	"fmt"
 	"strings"
 
 	environs "github.com/zen-io/zen-core/environments"
 	zen_targets "github.com/zen-io/zen-core/target"
+	"github.com/zen-io/zen-core/utils"
 )
 
 type ExecScript struct {
-	Pre     []string `mapstructure:"pre"`
-	Command []string `mapstructure:"command"`
-	Post    []string `mapstructure:"post"`
-	Deps    []string `mapstructure:"deps"`
+	Pre     []string          `mapstructure:"pre"`
+	Command []string          `mapstructure:"command"`
+	Post    []string          `mapstructure:"post"`
+	Deps    []string          `mapstructure:"deps"`
+	Env     map[string]string `mapstructure:"env"`
 }
 
 type ExecConfig struct {
-	BuildCommand    []string                         `mapstructure:"build"`
-	Srcs            []string                         `mapstructure:"srcs" desc:"Sources for the build"`
-	MappedSrcs      map[string][]string              `mapstructure:"mapped_srcs" desc:"a map of organised srcs"`
-	Outs            []string                         `mapstructure:"outs" desc:"Outs for the build"`
-	ScriptCommands  map[string]ExecScript            `mapstructure:"scripts"`
-	ExternalPath    *string                          `mapstructure:"external_path"`
-	Name            string                           `mapstructure:"name" desc:"Name for the target"`
-	Description     string                           `mapstructure:"desc" desc:"Target description"`
-	Labels          []string                         `mapstructure:"labels" desc:"Labels to apply to the targets"`
-	Deps            []string                         `mapstructure:"deps" desc:"Build dependencies"`
-	PassEnv         []string                         `mapstructure:"pass_env" desc:"List of environment variable names that will be passed from the OS environment, they are part of the target hash"`
-	SecretEnv       []string                         `mapstructure:"secret_env" desc:"List of environment variable names that will be passed from the OS environment, they are not used to calculate the target hash"`
-	Env             map[string]string                `mapstructure:"env" desc:"Key-Value map of static environment variables to be used"`
-	Tools           map[string]string                `mapstructure:"tools" desc:"Key-Value map of tools to include when executing this target. Values can be references"`
-	Visibility      []string                         `mapstructure:"visibility" desc:"List of visibility for this target"`
-	NoInterpolation bool                             `mapstructure:"no_interpolation" desc:"Do not interpolate when building"`
-	Environments    map[string]*environs.Environment `mapstructure:"environments" desc:"Deployment Environments"`
+	BuildCommand         []string                         `mapstructure:"build"`
+	Srcs                 []string                         `mapstructure:"srcs" desc:"Sources for the build"`
+	MappedSrcs           map[string][]string              `mapstructure:"mapped_srcs" desc:"a map of organised srcs"`
+	Outs                 []string                         `mapstructure:"outs" desc:"Outs for the build"`
+	ScriptCommands       map[string]ExecScript            `mapstructure:"scripts"`
+	ExternalPath         *string                          `mapstructure:"external_path"`
+	Name                 string                           `mapstructure:"name" zen:"yes" desc:"Name for the target"`
+	Description          string                           `mapstructure:"desc" zen:"yes" desc:"Target description"`
+	Labels               []string                         `mapstructure:"labels" zen:"yes" desc:"Labels to apply to the targets"`
+	Deps                 []string                         `mapstructure:"deps" zen:"yes" desc:"Build dependencies"`
+	PassEnv              []string                         `mapstructure:"pass_env" zen:"yes" desc:"List of environment variable names that will be passed from the OS environment, they are part of the target hash"`
+	PassSecretEnv        []string                         `mapstructure:"secret_env" zen:"yes" desc:"List of environment variable names that will be passed from the OS environment, they are not used to calculate the target hash"`
+	Env                  map[string]string                `mapstructure:"env" zen:"yes" desc:"Key-Value map of static environment variables to be used"`
+	Tools                map[string]string                `mapstructure:"tools" zen:"yes" desc:"Key-Value map of tools to include when executing this target. Values can be references"`
+	Visibility           []string                         `mapstructure:"visibility" zen:"yes" desc:"List of visibility for this target"`
+	NoCacheInterpolation bool                             `mapstructure:"no_interpolation" zen:"yes" desc:"Do not interpolate when building"`
+	Environments         map[string]*environs.Environment `mapstructure:"environments" zen:"yes" desc:"Deployment Environments"`
 }
 
-func (ec ExecConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_targets.Target, error) {
-	opts := []zen_targets.TargetOption{
-		zen_targets.WithSrcs(map[string][]string{"_srcs": ec.Srcs}),
-		zen_targets.WithOuts(ec.Outs),
-		zen_targets.WithVisibility(ec.Visibility),
-		zen_targets.WithEnvVars(ec.Env),
-		zen_targets.WithTools(ec.Tools),
-		zen_targets.WithPassEnv(ec.PassEnv),
-		zen_targets.WithEnvironments(ec.Environments),
-	}
-
-	if ec.NoInterpolation {
-		opts = append(opts, zen_targets.WithNoInterpolation())
-	}
-
+func (ec ExecConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_targets.TargetBuilder, error) {
 	if ec.ExternalPath != nil {
 		interpolatedPath, err := tcc.Interpolate(*ec.ExternalPath)
 		if err != nil {
 			return nil, fmt.Errorf("interpolating external path (%s): %w", *ec.ExternalPath, err)
 		}
-		opts = append(opts, zen_targets.WithExternalPath(interpolatedPath))
+		ec.ExternalPath = utils.StringPtr(interpolatedPath)
 	}
+
+	tb := zen_targets.ToTarget(ec)
+	tb.Srcs = map[string][]string{"_srcs": ec.Srcs}
+	tb.Outs = ec.Outs
+	tb.Labels = append(tb.Labels, fmt.Sprintf("cmd=%s", b64.StdEncoding.EncodeToString([]byte(strings.Join(ec.BuildCommand, ";")))))
 
 	if ec.ScriptCommands == nil {
 		ec.ScriptCommands = map[string]ExecScript{}
@@ -68,13 +62,17 @@ func (ec ExecConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_ta
 		Post:    nil,
 		Deps:    ec.Deps,
 	}
+
 	for script, execCmd := range ec.ScriptCommands {
-		if len(execCmd.Command) == 0 {
+		script := script
+		execCmd := execCmd
+		if script != "build" && len(execCmd.Command) == 0 {
 			return nil, fmt.Errorf("no commands provided for %s", script)
 		}
 
-		ts := &zen_targets.TargetScript{
+		tb.Scripts[script] = &zen_targets.TargetBuilderScript{
 			Deps: execCmd.Deps,
+			Env: execCmd.Env,
 			Pre: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
 				if execCmd.Pre != nil {
 					if pre, err := getCmd(execCmd.Pre, target); err != nil {
@@ -111,16 +109,9 @@ func (ec ExecConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_ta
 				return nil
 			},
 		}
-
-		opts = append(opts, zen_targets.WithTargetScript(script, ts))
 	}
 
-	return []*zen_targets.Target{
-		zen_targets.NewTarget(
-			ec.Name,
-			opts...,
-		),
-	}, nil
+	return []*zen_targets.TargetBuilder{tb}, nil
 }
 
 func getCmd(cmds []string, target *zen_targets.Target) ([]string, error) {
